d9d86d782168f43a2a90a9d55d78e196ea99a04d
lxhfight <lxhfight1@gmail.com>
2021-07-05 15:50:55 +0800

    新增图片描述

--- src/hxui/cpts/HxImage.vue
+++ src/hxui/cpts/HxImage.vue
@@ -1,8 +1,17 @@
+<!--
+ * @Author       : liuxuhao
+ * @LastEditors  : liuxuhao
+-->
 <template>
-  <div :class="['hx-image', (round && 'round'), position, (isBlock && 'block')]" 
-    :style="`height: ${height || ''}; width: ${width || ''}`" 
+  <div :class="['hx-image', 
+      (round && 'round'), 
+      position, 
+      (isBlock && 'block'),
+      (height && width) ? 'adjust' : ''
+    ]" 
+    :style="height && width ? `height: ${height}; width: ${width}` : ''" 
     ref="hxImage">
-    <img class="img" ref="img" v-if="!isLoadingImage" :src="src" alt="">
+    <img ref="img" :style="imageStyle" class="img" v-if="!isLoadingImage" :src="src" alt="">
   </div>
 </template>
 
@@ -49,14 +58,24 @@ export default {
       this.$_initImage()
     },
     $_getPositionType (height, width) { // 计算图片所需展示类型
+      if (!this.height || !this.width) {
+        return
+      }
       const $view = this.$refs.hxImage
       const cptRate = $view.clientHeight / $view.clientWidth // 组件的高宽比
-      const imgRate = height / width // 图片文件高宽比
-      if (imgRate > cptRate) {
-        this.position = 'centerVertical'
-      } else {
-        this.position = 'centerHorizontal'
-      }
+      this.$nextTick(() => {
+        const imgRate = height / width // 图片文件高宽比
+        const $image = this.$refs['img']
+        if (imgRate > cptRate) {
+          this.position = 'centerVertical'
+          $image.style.width = this.width
+          $image.style.height = 'auto'
+        } else {
+          this.position = 'centerHorizontal'
+          $image.style.height = this.height
+          $image.style.width = 'auto'
+        }
+      })
     },
     $_initImage () {
       this.isLoadingImage = true
@@ -70,6 +89,20 @@ export default {
   },
   mounted () {
     this.$_initLayout()
+  },
+  computed: {
+    imageStyle () {
+      if (this.height && this.width) {
+        return ''
+      }
+      if (this.height && !this.width) {
+        return `width: auto; height: ${this.height};`
+      } else if (!this.height && this.width) {
+        return `height: auto; width: ${this.width};`
+      } else {
+        return `height: auto; width: auto;`
+      }
+    }
   },
   watch: {
     src (newVal, oldVal) {
--- src/views/components/basic/image.vue
+++ src/views/components/basic/image.vue
@@ -4,22 +4,44 @@
 -->
 <template>
   <article class="pad-article">
-    <header class="title">组件名 Demo For Copy</header>
+    <header class="title">图片 HxImage</header>
     <span class="desc">
-      整体描述写于此 （<em>关键词</em>） 
+      <strong>HxImage</strong>图片组件主要是用于弥补 <em>img</em> 图片标签上的一些体验上的不足，主要增加了包括加载中效果和自定义填充的特性。 
     </span>
     <div class="section">
-      <span class="title">属性名</span>
-      <span class="desc">属性描述[<strong>重点</strong>]</span>
+      <span class="title">常规使用</span>
+      <span class="desc">
+        类似 <strong>img</strong> 标签用法，
+        也是通过 <em>src</em> 传入图片链接，通过 <em>alt</em> 设置出错时展示文本内容。
+      </span>
       <div class="pad-preview">
-        <div class="demo">
-          展示区
+        <div class="demo"> 
+          <div class="hx-pad-left" style="height: 132px;">
+            <hx-row label="定高不定宽：" label-size="bg">
+              <hx-image height="100px" src="https://static.lxhfight.com/img/image-candy.jpg"></hx-image>
+              <span class="tips" slot="tips">仅高度设置为100px</span>
+            </hx-row>
+          </div>
+          <div class="hx-pad-right" style="height: 132px;">
+            <hx-row label="定宽不定高：" label-size="bg">
+              <hx-image width="132px" src="https://static.lxhfight.com/img/image-candy.jpg"></hx-image>
+              <span class="tips" slot="tips">仅宽度设置为132px</span>
+            </hx-row>
+          </div>
+          <hx-row label="定宽定宽智能展示：" label-size="bg">
+            <hx-image height="100px" width="300px"  src="https://static.lxhfight.com/img/image-candy.jpg"></hx-image>
+            <span class="tips" slot="tips">高度定义100px，宽度定义340px, 此时智能展示垂直方向中央部分</span>
+          </hx-row>
+          <hx-row label="定宽定宽智能展示：" label-size="bg">
+            <hx-image height="100px" width="70px"  src="https://static.lxhfight.com/img/image-candy.jpg"></hx-image>
+            <span class="tips" slot="tips">高度定义100px，宽度定义70px，此时展示水平方向中央部分</span>
+          </hx-row>
         </div>
         <div class="code">
           <span class="tips">
-            代码中附带的一些小提示
+            分别对应：定高不定宽、定宽不定高、定高定宽且宽高比例大于和小于图片宽高比共四种场景。
           </span>
-          <pre class="detail" v-highlightjs="texts[0]" >
+          <pre class="detail" v-highlightjs="texts[0]">
             <code class="xml">
             </code>
           </pre>
@@ -36,17 +58,22 @@
   </article>
 </template>
 <script>
+import { HxImage } from '@/hxui'
 import EventsTable from '@/views/cpts/EventsTable'
 import PropsTable from '@/views/cpts/PropsTable'
 export default {
   components: {
+    HxImage,
     EventsTable,
     PropsTable
   },
   data () {
     return {
       texts: [
-        `代码演示区1`
+        `<hx-image height="100px" src="https://static.lxhfight.com/img/image-candy.jpg"></hx-image>
+<hx-image width="132px" src="https://static.lxhfight.com/img/image-candy.jpg"></hx-image>
+<hx-image height="100px" width="300px" src="https://static.lxhfight.com/img/image-candy.jpg"></hx-image>
+<hx-image height="100px" width="70px" src="https://static.lxhfight.com/img/image-candy.jpg"></hx-image>`
       ],
       props: [
         { name: 'content', desc: '下拉菜单选项内容', type: 'Array', option: '由数值、字符串或对象组成', default: '[]' },
